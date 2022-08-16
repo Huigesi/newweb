59de1591299cae325d6617a5c5bca85e808913d1
Huigesi <791339970@qq.com>
2022-08-16 16:10:51 +0800

    change !

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1> hello, I am KLQkuailequan</h1>")
+	io.WriteString(w, "<h1> hello, I am KLQkuailequan!!!</h1>")
 }
 
 func main() {
